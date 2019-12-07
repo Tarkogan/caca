906e4852c84bf9e9929f24ef372b33ff097fc63e
DUBOIN Léo <leo.duboin@epita.fr>
2019-12-07 03:38:55 +0100

    2+2=3

--- Source/builtin/generate_primes.ml
+++ Source/builtin/generate_primes.ml
@@ -1,5 +1,10 @@
 (** Generating primes *)
 
+(*
+#mod_use "builtin.ml";;
+#mod_use "basic_arithmetics.ml";;
+*)
+
 open Builtin
 open Basic_arithmetics
 
@@ -10,27 +15,50 @@ open Basic_arithmetics
 (** List composed of 2 and then odd integers starting at 3.
     @param n number of elements in the list of integers.
  *)
-let init_eratosthenes n = []
+let init_eratosthenes n =
+  let rec truc i = match i with
+    |x when x > n -> []
+    |x -> x::truc(i+2)
+  in
+  2::truc 3;;
 
 (* Eratosthenes sieve. *)
 
 (** Eratosthene sieve.
     @param n limit of list of primes, starting at 2.
 *)
-let eratosthenes n = []
+let eratosthenes n =  
+  let rec is_prime nbr i = match modulo nbr i with
+    |_ when i*i > abs(nbr) -> true
+    |0 -> false
+    |_ -> is_prime nbr (i+2)
+  in
+  let rec test i = match i with
+    |x when x > n -> []
+    |x -> if is_prime i 3  then i::test(i+2) else test(i+2)
+  in
+  2::test 3;;
 
 (* Write and read into file functions for lists. *)
 
 (** Write a list into a file. Element seperator is newline.
     @param file path to write to.
  *)
-let write_list li file = ()
+let write_list li file =
+  let file = open_out file in
+  let rec write_in liste fichier = match liste with
+    |[] -> close_out file;
+    |e::s -> Printf.fprintf fichier "%d\n" e; write_in s fichier
+  in  
+  write_in li file;;
+  
 
 (** Write a list of prime numbers up to limit into a txt file.
     @param n limit of prime numbers up to which to build up a list of primes.
     @param file path to write to.
 *)
-let write_list_primes n file = ()
+let write_list_primes n file =
+  let liste = eratosthenes n in write_list liste file;;
 
 (** Read file safely ; catch End_of_file exception.
     @param in_c input channel.
@@ -42,12 +70,19 @@ let input_line_opt in_c =
 (** Create a list out of reading a line per line channel.
     @param in_c input channel.
  *)
-let create_list in_c = ()
-
+let create_list in_c =
+  let try_read() = input_line_opt in_c
+  in
+  let rec listing() = match try_read() with
+    |Some s -> int_of_string(s)::(listing())
+    |None -> close_in in_c;[]
+  in
+  listing();;
+      
 (** Load list of primes into OCaml environment.
     @param file path to load from.
  *)
-let read_list_primes file = []
+let read_list_primes file = let file = open_in file in create_list file;;
 
 (* Auxiliary functions to extract big prime numbers for testing
    purposes.
@@ -74,17 +109,28 @@ let rec last_two l = match l with
 
 (* Generating couples of prime numbers for specific or fun
    purposes.
- *)
+*)
+
+(*
+creates a list of couples of prime numbers linked to eachother by the relation f
+*)
+let prime_couples limit isprime f =
+  let prime_list = eratosthenes limit in
+  let rec couple liste = match liste with
+    |[] -> []
+    |e::s -> let nbr = f(e) in if not(isprime(nbr)) then couple s else (e,nbr)::couple s
+  in
+  couple prime_list;;
 
 (** Finding couples of primes where second entry is twice the first
     plus 1.
     @param limit positive integer bounding searched for primes.
     @param isprime function testing for (pseudo)primality.
  *)
-let double_primes limit isprime = []
+let double_primes limit isprime = prime_couples limit isprime (function x -> 2*x+1);;
 
 (** Finding twin primes.
     @param limit positive integer bounding searched for primes.
     @param isprime function testing for (pseudo)primality.
  *)
-let twin_primes limit isprime = []
+let twin_primes limit isprime = prime_couples limit isprime (function x -> x+2);;

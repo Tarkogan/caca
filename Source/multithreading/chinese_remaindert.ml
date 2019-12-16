4c7025008ed7adbe37ac472900268dbb2e34c1f5
DUBOIN Léo <leo.duboin@epita.fr>
2019-12-16 02:58:09 +0100

    CRT: What am i suppposed to do ?

--- Source/multithreading/chinese_remaindert.ml
+++ Source/multithreading/chinese_remaindert.ml
@@ -1,13 +1,29 @@
 (** Chinese remainder theorem *)
 
+(*
+#directory "../builtin";;
+#mod_use "builtin.ml";;
+#mod_use "basic_arithmetics.ml";;
+#mod_use "power.ml";;
+#mod_use "test_primes.ml";;
+*)
+
 open Builtin
 open Basic_arithmetics
+open Power
+open Test_primes
 
-(** Image of the Chinese Remainder map
+(** Image of the Chinese Remainder map (list)
     @param x positive integer of which you take image
     @param l list of pairwise relatively prime positive integers.
  *)
-let crt_image x l = []
+let crt_image x l =
+  let rec image list = match list with
+    |[] -> []
+    |e::s -> modulo x e :: image s
+  in
+  image l;;
+  
 
 (** Inverse image of Chinese Remainder map
     @para m a positive integer
